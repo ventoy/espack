d35750fc50e002206a122a1c7c6c2e7e623529e4
BxOxSxS <BxOxSxS@protonmail.com>
2021-08-19 14:53:42 +0200

    Add i486 and i586 machine platform for linux

--- esp.sh
+++ esp.sh
@@ -2,7 +2,7 @@
 
 if uname -m | egrep -q 'amd64|x86_64'; then
     espack=./tool/espack64
-elif uname -m | egrep -q 'i386|i686'; then
+elif uname -m | egrep -q 'i[3-6]86'; then
     espack=./tool/espack32
 else
     echo Unsupported machine platform: $(uname -m)
