a2e972e21089f136fb26b7e0ce7efa9652674230
BxOxSxS <BxOxSxS@protonmail.com>
2021-08-19 12:35:48 +0200

    Improved machine platform detection on linux

--- esp.sh
+++ esp.sh
@@ -2,8 +2,11 @@
 
 if uname -m | egrep -q 'amd64|x86_64'; then
     espack=./tool/espack64
-else
+elif uname -m | egrep -q 'i386|i686'; then
     espack=./tool/espack32
+else
+    echo Unsupported machine platform: $(uname -m)
+    exit 1
 fi
 
 chmod +x $espack
